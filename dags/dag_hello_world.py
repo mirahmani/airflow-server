46027d9c3d39ca100fc4060f841a238a9874498f
Miranti Rahmani <mirantirahmani@Mirantis-MacBook-Pro.local>
2024-03-03 11:40:46 +0700

    First DAG for testing deployment to server

---
+++ dags/dag_hello_world.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def print_hello():
+    return 'Hello'
+
+def print_world():
+    print('World')
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 3, 2),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('hello_world',
+          default_args=default_args,
+          description='A simple HelloWorld DAG',
+          schedule_interval=timedelta(days=1),
+          catchup=False,
+          )
+
+start_task = DummyOperator(
+    task_id='start_task',
+    dag=dag,
+)
+
+hello_task = PythonOperator(
+    task_id='hello_task',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+world_task = PythonOperator(
+    task_id='world_task',
+    python_callable=print_world,
+    dag=dag,
+)
+
+start_task >> hello_task >> world_task
