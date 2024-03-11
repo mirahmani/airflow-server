5c3dd40d1f395d73588566a22a773d217bf6ee30
Miranti Rahmani <mirantirahmani@Mirantis-MacBook-Pro.local>
2024-03-11 13:26:28 +0700

    Change back the unittest

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,23 +1,28 @@
-import pytest
+import unittest
 from airflow.models import DagBag
 
-@pytest.fixture(scope="session")
-def dag_bag():
-    return DagBag(dag_folder="dags/", include_examples=False)
+class TestExampleDAG(unittest.TestCase):
 
-def test_dag_loaded(dag_bag):
-    """Check that the DAG file is correctly imported into the DagBag"""
-    assert 'example_dag' in dag_bag.dags
-    assert len(dag_bag.dags['example_dag'].tasks) > 0
+    @classmethod
+    def setUpClass(cls):
+        cls.dagbag = DagBag(dag_folder="dags/", include_examples=False)
 
-def test_task_dependencies(dag_bag):
-    """Check the task dependencies in the example_dag"""
-    dag = dag_bag.get_dag(dag_id='example_dag')
-    start_task = dag.get_task('start_task')
-    hello_task = dag.get_task('hello_task')
-    world_task = dag.get_task('world_task')
+    def test_dag_loaded(self):
+        """Check that the DAG file is correctly imported into the DagBag"""
+        self.assertIn('example_dag', self.dagbag.dags)
+        self.assertGreater(len(self.dagbag.dags['example_dag'].tasks), 0)
 
-    assert start_task.downstream_task_ids == {'hello_task'}
-    assert hello_task.upstream_task_ids == {'start_task'}
-    assert hello_task.downstream_task_ids == {'world_task'}
-    assert world_task.upstream_task_ids == {'hello_task'}
+    def test_task_dependencies(self):
+        """Check the task dependencies in the example_dag"""
+        dag = self.dagbag.get_dag(dag_id='example_dag')
+        start_task = dag.get_task('start_task')
+        hello_task = dag.get_task('hello_task')
+        world_task = dag.get_task('world_task')
+
+        self.assertEqual(start_task.downstream_task_ids, {'hello_task'})
+        self.assertEqual(hello_task.upstream_task_ids, {'start_task'})
+        self.assertEqual(hello_task.downstream_task_ids, {'world_task'})
+        self.assertEqual(world_task.upstream_task_ids, {'hello_task'})
+
+if __name__ == '__main__':
+    unittest.main()
